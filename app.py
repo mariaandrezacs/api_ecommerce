00c43262b0300f33057ef6b3e1f5f45d3d1bef59
Maria Andreza C Santos <mandreza008@gmail.com>
2024-11-09 21:38:43 -0300

    feat: add table db product

--- app.py
+++ app.py
@@ -6,4 +6,10 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///ecommerce.db'
 
 db = SQLAlchemy(app)
 
+# Produto (id, name, price, description)
 
+class Product(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), nullable=False)
+    price = db.Column(db.Float, nullable=False)
+    description = db.Column(db.Text, nullable=True)
