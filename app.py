02ca13ee66974d591449d1f1a203b15d83bb808b
Maria Andreza C Santos <mandreza008@gmail.com>
2024-11-09 21:33:37 -0300

    Initial Commit

---
+++ app.py
@@ -0,0 +1,9 @@
+from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///ecommerce.db'
+
+db = SQLAlchemy(app)
+
+
